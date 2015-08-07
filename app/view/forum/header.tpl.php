c3e61a900fc9565db6623e3e0799ae5793467278
Jonas Ahlsson <jonasahlsson@hotmail.com>
2015-08-08 00:14:18 +0200

    header

--- app/view/forum/header.tpl.php
+++ app/view/forum/header.tpl.php
@@ -1,3 +1,5 @@
 <img class='sitelogo' src='<?=$this->url->asset("img/home.png")?>' alt='Logo. A house'/>
-<span class='sitetitle'><?=$siteTitle?></span>
-<span class='siteslogan'><?=$siteTagline?></span>
+<div class='warp-title'>
+    <div><span class='sitetitle'><?=$siteTitle?></span></div>
+    <div><span class='siteslogan'><?=$siteTagline?></span></div>
+</div>
